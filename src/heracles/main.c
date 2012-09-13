12b761d4fa133ffb18766edd1346e77dc68d1a7e
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2012-09-13 03:00:11 +0200

    Inefficient but simple command parser (parsing input commands is not critical)

--- src/heracles/main.c
+++ src/heracles/main.c
@@ -24,6 +24,8 @@
 
 #include "gpio.h"
 #include "usart.h"
+#include <string.h>
+#include <stdio.h>
 
 
 // TODO: avoid using wirish stuff
@@ -33,11 +35,58 @@ __attribute__((constructor)) void premain() {
 }
 
 
+#define BUFFER_SIZE 64
+
 const char *dummy_str = "Hello!\n";
+char buffer[BUFFER_SIZE];
+char *p_buffer;
+
+
+int set_speed(int speed_left, int speed_right)
+{
+	// Set speed left
+	if (speed_left < 0) {
+		gpio_write_bit(GPIOA, 6, 1);
+		gpio_write_bit(GPIOA, 7, 0);
+		speed_left = -speed_left;
+	} else {
+		gpio_write_bit(GPIOA, 6, 0);
+		gpio_write_bit(GPIOA, 7, 1);
+	}
+	pwmWrite(3, (speed_left * 65535) / 10); // TODO: avoid using wirish stuff!!
+	// TODO: Set speed right
+	return 0;
+}
+
+int parse_command(char *buffer)
+{
+	char *p_buffer;
+
+	p_buffer = buffer;
+
+	if (!strncmp(p_buffer, "at+", 3)) {
+		p_buffer += 3;
+		if (!strncmp(p_buffer, "speed:", 3)) {
+			p_buffer += 6;
+			int speed_left;
+			int speed_right;
+			if (sscanf(p_buffer, "%d,%d", &speed_left, &speed_right) != 2) return 1;
+			else set_speed(speed_left, speed_right);
+		} else return 1;
+	} else return 1;
+
+	return 0;
+}
 
 
 void setup(void)
 {
+	p_buffer = (char *) buffer;
+
+	gpio_set_mode(GPIOA, 6, GPIO_OUTPUT_PP);
+	gpio_set_mode(GPIOA, 7, GPIO_OUTPUT_PP);
+	pinMode(3, PWM);                     // TODO: avoid using wirish stuff
+
 	// Set output mode in D33 (Maple Mini's SMD LED) and set it to high
 	gpio_set_mode(GPIOB, 1, GPIO_OUTPUT_PP);
 	gpio_write_bit(GPIOB, 1, 1);
@@ -75,11 +124,16 @@ void loop(void)
 
 	while (usart_data_available(USART1)) {
 
-		uint8 input = usart_getc(USART1);
+		uint8 c_input = usart_getc(USART1);
 
-		usart_putstr(USART1, "Received: ");
-		usart_putstr(USART1, (const char *) &input);
-		usart_putstr(USART1, "\n");
+		*p_buffer = (char) c_input;
+		p_buffer++;
+
+		if (c_input == (uint8) '\n') {
+			*p_buffer = '\0';
+			p_buffer = (char *) buffer;
+			if (parse_command(buffer)) usart_putstr(USART1, "Unknown command!\n");;
+		}
 
 	}
 }
